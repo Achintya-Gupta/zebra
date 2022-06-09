b2f0fb6b84c75564d77a7131c0b5d856a69cc8ab
Achintya-Gupta <88548454+Achintya-Gupta@users.noreply.github.com>
2022-06-09 11:15:46 +0530

    Add files via upload

---
+++ Lets chat web app/kwitter.js
@@ -0,0 +1,6 @@
+function  addRoom(){
+    room_name = document.getElementById("room_name").value
+    firebase.database().ref("/").child(room_name).update({ purpose : "adding room name" });
+    localStorage.setItem("room name" , room_name)
+    window.location("kwitter_room.html")
+  }
